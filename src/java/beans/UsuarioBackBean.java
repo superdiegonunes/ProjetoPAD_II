1598a90b55217511f523b6fba1d08a5b825e24aa
Gilberto Augusto <gilberto.bastos@outlook.com.br>
2016-11-22 10:53:55 -0200

    Commit para corrigir conflitos de um merge anterior principalmente na classe "UsuarioBackBean".

--- src/java/beans/UsuarioBackBean.java
+++ src/java/beans/UsuarioBackBean.java
@@ -10,49 +10,46 @@ import dao.UsuarioDAO;
 @ManagedBean(name = "usuarioBackBean")
 @SessionScoped
 public class UsuarioBackBean {
-    
+
     private static UsuarioDAO uDAO = new UsuarioDAO();
     private String message;
     private Usuario usuario = new Usuario();
-    
-    public UsuarioBackBean() {}
-    
+
+    public UsuarioBackBean() {
+    }
+
     public String logar() throws SQLException, ClassNotFoundException {
-        
+
         /* Tentando logar. */
         String login = usuario.getLogin();
         String senha = usuario.getSenha();
         Usuario usuario = uDAO.autenticarUsuario(login, senha);
-        
+
         if (usuario != null) {
-<<<<<<< HEAD
-            if (usuario.isAdministrador() == true){
-=======
-          
+
             if (usuario.isAdministrador()) {
->>>>>>> origin/master
                 return "/cadastroItem";
             }
-            
+
             return "/carrinho";
         }
-        
+
         message = "Login/senha inválidos!";
         return null;
     }
-    
+
     public String getMessage() {
         return message;
     }
-    
+
     public void setMessage(String message) {
         this.message = message;
     }
-    
+
     public Usuario getUsuario() {
         return usuario;
     }
-    
+
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
@@ -61,24 +58,24 @@ public class UsuarioBackBean {
      * Método que cadastra o usuário no banco de dados.
      *
      * @return Action.
-     * 
+     *
      * @throws java.sql.SQLException
      * @throws java.lang.ClassNotFoundException
      */
     public String cadastrarUsuarioBanco() throws SQLException, ClassNotFoundException {
         /* Cadastrando o usuário. */
         uDAO.adicionar(usuario);
-        
+
         /* Retornando para a página principal. */
         return "/index";
     }
-    
+
     public String sair() {
         FacesContext.getCurrentInstance().getExternalContext().
                 invalidateSession();
         return "/index";
     }
-    
+
     public String voltar() {
         return "/index";
     }
